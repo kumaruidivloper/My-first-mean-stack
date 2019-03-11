6e3ad2ba3aa79addb0d3d1796b05c946de2058db
Kumaravel Shanmugam <M1050211@mindtree.com>
2019-03-11 12:00:07 +0530

    auth-started

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,14 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
 mongoose
   .connect(
-    "mongodb+srv://Test001:SqmcAZrgtOf3FtG4@kumar001-dv4pg.mongodb.net/node-angular?retryWrites=true"
+    // "mongodb+srv://Test001:SqmcAZrgtOf3FtG4@kumar001-dv4pg.mongodb.net/node-angular?retryWrites=true"
+    "mongodb+srv://Test002:OJmrw0WhWJe0McLD@kumar002-myr2k.mongodb.net/node-angular"
   )
   .then(() => {
     console.log("Connected to database!");
@@ -36,5 +38,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
 
 module.exports = app;
